aafe43ca5cd8b769fc2ba523a40edd599c139c77
erkan33-byte <erkan_33@live.nl>
2026-06-29 15:58:36 +0200

    Update build.sh

--- wasm/build.sh
+++ wasm/build.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 set -e
 
-# Installeer Emscripten
+# 1. Installeer Emscripten (de compiler)
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk
 ./emsdk install latest
 ./emsdk activate latest
 source ./emsdk_env.sh
 
-# Compileer WASM bestanden
-emcc secp256k1.cpp -O3 -s WASM=1 -s EXPORTED_FUNCTIONS='["_mul_G","_add_G","_randomInRange","_getPublicKey"]' -o secp256k1.wasm
-emcc crypto.cpp -O3 -s WASM=1 -s EXPORTED_FUNCTIONS='["_hash160","_doubleSha256"]' -o crypto.wasm
-emcc base58.cpp -O3 -s WASM=1 -s EXPORTED_FUNCTIONS='["_encode","_encodeWIF"]' -o base58.wasm
+# 2. Compileer secp256k1 (met echte elliptic curve code)
+emcc secp256k1.cpp -O3 \
+    -s WASM=1 \
+    -s EXPORTED_FUNCTIONS='["_mul_G","_add_G","_randomInRange","_getPublicKey"]' \
+    -s EXPORTED_RUNTIME_METHODS='["cwrap"]' \
+    -o secp256k1.wasm
 
-# Maak output map
+# 3. Compileer crypto (SHA256 + RIPEMD160)
+emcc crypto.cpp -O3 \
+    -s WASM=1 \
+    -s EXPORTED_FUNCTIONS='["_hash160","_doubleSha256"]' \
+    -s EXPORTED_RUNTIME_METHODS='["cwrap"]' \
+    -o crypto.wasm
+
+# 4. Compileer base58
+emcc base58.cpp -O3 \
+    -s WASM=1 \
+    -s EXPORTED_FUNCTIONS='["_encode","_encodeWIF"]' \
+    -s EXPORTED_RUNTIME_METHODS='["cwrap"]' \
+    -o base58.wasm
+
+# 5. Verplaats de wasm bestanden naar de docs map (voor GitHub Pages)
 mkdir -p ../docs/wasm
-cp *.wasm ../docs/wasm/
+mv *.wasm ../docs/wasm/
